6b81ab52517ab690772df6171908f7645bec200d
jiakai <jiakai@bytedance.com>
2018-04-20 21:55:52 +0800

    update 2>&1
    Change-Id: I15ff157100226d42a7e10883db18183dcd4ff1fc

--- monte_carlo_policy_gradient/mountaincarv0/batch.sh
+++ monte_carlo_policy_gradient/mountaincarv0/batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../../search_params/gen.py | awk '{print "python run_mountaincarv0.py --alpha "$0" > tunning."$0" &"}' | sh
+python ../../search_params/gen.py | awk '{print "python run_mountaincarv0.py --alpha "$0" > tunning."$0" 2>&1 &"}' | sh
 
