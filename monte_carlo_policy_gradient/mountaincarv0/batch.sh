cd3dfae3ac7e91991d403aeeb71427c970eb737e
jiakai <jiakai@bytedance.com>
2018-04-21 17:43:20 +0800

    MountainCarv0 减少搜索范围
    Change-Id: I4550ed2ac99f13807747b701dd4a14ec5eeebb65

--- monte_carlo_policy_gradient/mountaincarv0/batch.sh
+++ monte_carlo_policy_gradient/mountaincarv0/batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../../search_params/gen.py | awk '{print "nohup python run_mountaincarv0.py --alpha "$0" > tunning."$0" 2>&1 &"}' | sh
+python ../../search_params/gen.py | awk '{print "nohup python run_mountaincarv0.py --alpha "$0" > tuning."$0" 2>&1 &"}' | sh
 
--- search_params/gen.py
+++ search_params/gen.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-N = 10
+N = 3
 
-if __name__ == '__main__':
+
+def universal():
     for _ in range(N):
         # print np.random.uniform(low=0.1, high=1.0)
         print np.random.uniform(low=0.01, high=0.1)
@@ -12,3 +13,19 @@ if __name__ == '__main__':
         print np.random.uniform(low=0.000001, high=0.00001)
         # print np.random.uniform(low=0.0000001, high=0.000001)
         # print np.random.uniform(low=0.00000001, high=0.0000001)
+
+
+def mountaincarv0():
+    for _ in range(N):
+        # print np.random.uniform(low=0.1, high=1.0)
+        print np.random.uniform(low=0.01, high=0.1)
+        print np.random.uniform(low=0.001, high=0.01)
+        print np.random.uniform(low=0.0001, high=0.001)
+        print np.random.uniform(low=0.00001, high=0.0001)
+        # print np.random.uniform(low=0.000001, high=0.00001)
+        # print np.random.uniform(low=0.0000001, high=0.000001)
+        # print np.random.uniform(low=0.00000001, high=0.0000001)
+
+
+if __name__ == '__main__':
+    mountaincarv0()
