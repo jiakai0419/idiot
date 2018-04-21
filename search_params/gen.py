326c38133bfd33acecbecea541eb316e3beee4fc
jiakai <jiakai@bytedance.com>
2018-04-22 00:16:39 +0800

    update gen
    Change-Id: I6cf84047155d855d2d9c0a0b020a6c63462112b6

--- search_params/gen.py
+++ search_params/gen.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-N = 3
+N = 30
 
 
 def universal():
@@ -21,7 +21,7 @@ def mountaincarv0():
         print np.random.uniform(low=0.01, high=0.1)
         print np.random.uniform(low=0.001, high=0.01)
         print np.random.uniform(low=0.0001, high=0.001)
-        print np.random.uniform(low=0.00001, high=0.0001)
+        # print np.random.uniform(low=0.00001, high=0.0001)
         # print np.random.uniform(low=0.000001, high=0.00001)
         # print np.random.uniform(low=0.0000001, high=0.000001)
         # print np.random.uniform(low=0.00000001, high=0.0000001)
