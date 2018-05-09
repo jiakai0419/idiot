6a0313b39da7ce950a2a44f6087bd5e7c4f4c9f0
jiakai <jiakai@bytedance.com>
2018-05-09 23:40:39 +0800

    print_function
    Change-Id: Ifce758bde56283d0094787d9963a59e1e958e66c

--- search_params/gen.py
+++ search_params/gen.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 
 N = 1
@@ -5,38 +6,38 @@ N = 1
 
 def universal():
     for _ in range(N):
-        # print np.random.uniform(low=0.1, high=1.0)
-        print np.random.uniform(low=0.01, high=0.1)
-        print np.random.uniform(low=0.001, high=0.01)
-        print np.random.uniform(low=0.0001, high=0.001)
-        print np.random.uniform(low=0.00001, high=0.0001)
-        print np.random.uniform(low=0.000001, high=0.00001)
-        # print np.random.uniform(low=0.0000001, high=0.000001)
-        # print np.random.uniform(low=0.00000001, high=0.0000001)
+        # print(np.random.uniform(low=0.1, high=1.0))
+        print(np.random.uniform(low=0.01, high=0.1))
+        print(np.random.uniform(low=0.001, high=0.01))
+        print(np.random.uniform(low=0.0001, high=0.001))
+        print(np.random.uniform(low=0.00001, high=0.0001))
+        print(np.random.uniform(low=0.000001, high=0.00001))
+        # print(np.random.uniform(low=0.0000001, high=0.000001))
+        # print(np.random.uniform(low=0.00000001, high=0.0000001))
 
 
 def mcpg_mountaincarv0():
     for _ in range(N):
-        # print np.random.uniform(low=0.1, high=1.0)
-        print np.random.uniform(low=0.01, high=0.1)
-        print np.random.uniform(low=0.001, high=0.01)
-        print np.random.uniform(low=0.0001, high=0.001)
-        # print np.random.uniform(low=0.00001, high=0.0001)
-        # print np.random.uniform(low=0.000001, high=0.00001)
-        # print np.random.uniform(low=0.0000001, high=0.000001)
-        # print np.random.uniform(low=0.00000001, high=0.0000001)
+        # print(np.random.uniform(low=0.1, high=1.0))
+        print(np.random.uniform(low=0.01, high=0.1))
+        print(np.random.uniform(low=0.001, high=0.01))
+        print(np.random.uniform(low=0.0001, high=0.001))
+        # print(np.random.uniform(low=0.00001, high=0.0001))
+        # print(np.random.uniform(low=0.000001, high=0.00001))
+        # print(np.random.uniform(low=0.0000001, high=0.000001))
+        # print(np.random.uniform(low=0.00000001, high=0.0000001))
 
 
 def a3c_mountaincarv0():
     for _ in range(N):
-        # print np.random.uniform(low=0.1, high=1.0)
-        print np.random.uniform(low=0.01, high=0.1)
-        print np.random.uniform(low=0.001, high=0.01)
-        print np.random.uniform(low=0.0001, high=0.001)
-        # print np.random.uniform(low=0.00001, high=0.0001)
-        # print np.random.uniform(low=0.000001, high=0.00001)
-        # print np.random.uniform(low=0.0000001, high=0.000001)
-        # print np.random.uniform(low=0.00000001, high=0.0000001)
+        # print(np.random.uniform(low=0.1, high=1.0))
+        print(np.random.uniform(low=0.01, high=0.1))
+        print(np.random.uniform(low=0.001, high=0.01))
+        print(np.random.uniform(low=0.0001, high=0.001))
+        # print(np.random.uniform(low=0.00001, high=0.0001))
+        # print(np.random.uniform(low=0.000001, high=0.00001))
+        # print(np.random.uniform(low=0.0000001, high=0.000001))
+        # print(np.random.uniform(low=0.00000001, high=0.0000001))
 
 
 if __name__ == '__main__':
