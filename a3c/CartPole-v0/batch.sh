57c473134d14b7c25b2f06e7e13df7d710cd7953
KaiJia <jiakai0419@gmail.com>
2018-05-10 15:53:16 +0800

    ignore stdout stderr

--- a3c/CartPole-v0/batch.sh
+++ a3c/CartPole-v0/batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../../search_params/gen.py | awk '{print "nohup python main.py --lr "$0" --num-processes 1 --episode-num-per-proc 10000 > tuning."$0" 2>&1 &"}' | sh
+python ../../search_params/gen.py | awk '{print "nohup python main.py --lr "$0" --num-processes 1 --episode-num-per-proc 10000 > /dev/null 2>&1 &"}' | sh
 
