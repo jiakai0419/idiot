5b17170fbaf440062f745a2abb09fd7b03147bc9
jiakai <jiakai@bytedance.com>
2018-05-14 18:03:36 +0800

    batch a2c MountainCar-v0
    Change-Id: Ib91cd43e1548254cf0e91866b63c78838dc74729

--- a2c/MountainCar-v0/main.py
+++ a2c/MountainCar-v0/main.py
@@ -5,14 +5,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 MAGIC_NUMBER = 1103515245
 
 
 class ActorNet(torch.nn.Module):
     def __init__(self, state_dim, action_dim):
         super(ActorNet, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 20)
-        self.output = nn.Linear(20, action_dim)
+        self.fc1 = nn.Linear(state_dim, 12)
+        self.output = nn.Linear(12, action_dim)
 
     def forward(self, inputs):
         x = F.relu(self.fc1(inputs))
@@ -22,8 +27,8 @@ class ActorNet(torch.nn.Module):
 class CriticNet(torch.nn.Module):
     def __init__(self, state_dim):
         super(CriticNet, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 20)
-        self.output = nn.Linear(20, 1)
+        self.fc1 = nn.Linear(state_dim, 12)
+        self.output = nn.Linear(12, 1)
 
     def forward(self, inputs):
         x = F.relu(self.fc1(inputs))
@@ -33,10 +38,11 @@ class CriticNet(torch.nn.Module):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', default='MountainCar-v0')
-    parser.add_argument('--episode-num', type=int, default=4000)
+    parser.add_argument('--episode-num', type=int, default=8000)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--entropy-coef', type=float, default=0.01)
+    parser.add_argument('--t-max', type=int, default=20000)
     args = parser.parse_args()
 
     torch.manual_seed(MAGIC_NUMBER)
@@ -44,8 +50,8 @@ if __name__ == '__main__':
     env = gym.make(args.env_name)
     env = env.unwrapped
     env.seed(MAGIC_NUMBER)
-    print('[debug] observation_space:{}'.format(env.observation_space.shape))
-    print('[debug] action_space:{}'.format(env.action.shape))
+    print('[debug] observation_space.shape:{}'.format(env.observation_space.shape))
+    print('[debug] action_space.n:{}'.format(env.action_space.n))
 
     actor_net = ActorNet(env.observation_space.shape[0], env.action_space.n)
     critic_net = CriticNet(env.observation_space.shape[0])
@@ -53,11 +59,15 @@ if __name__ == '__main__':
     actor_optimizer = optim.Adam(actor_net.parameters(), lr=args.lr)
     critic_optimizer = optim.Adam(critic_net.parameters(), lr=args.lr)
 
+    performance_line = []
+
     for episode in range(args.episode_num):
         s = env.reset()
         s = torch.from_numpy(s).float()
         rewards = 0
+        t = 0
         while True:
+            t += 1
             logit = actor_net.forward(s.unsqueeze(0))
             prob = F.softmax(logit, dim=1)
 
@@ -68,6 +78,8 @@ if __name__ == '__main__':
             log_prob = log_prob.gather(1, action)
 
             ss, r, done, _ = env.step(action.item())
+            if args.t_max <= t:
+                done = True
             rewards += r
             if not done:
                 ss = torch.from_numpy(ss).float()
@@ -94,3 +106,14 @@ if __name__ == '__main__':
             s = ss
 
         print('episode:{}, rewards:{}'.format(episode + 1, rewards))
+
+        if (episode + 1) % 10 == 0:
+            performance_line.append((episode + 1, rewards))
+
+    # plot performance
+    fig, ax = plt.subplots()
+    ax.plot([p[0] for p in performance_line], [p[1] for p in performance_line])
+    ax.set_xlabel('episode')
+    ax.set_ylabel('rewards')
+    ax.set_title('PERFORMANCE last_rewards:{}'.format(performance_line[-1][1]))
+    fig.savefig('performance_alpha_{}.png'.format(args.lr))
--- search_params/gen.py
+++ search_params/gen.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import numpy as np
 
-N = 10
+N = 20
 
 
 def universal():
@@ -40,5 +40,17 @@ def a3c_mountaincarv0():
         # print(np.random.uniform(low=0.00000001, high=0.0000001))
 
 
+def a2c_mountaincarv0():
+    for _ in range(N):
+        # print(np.random.uniform(low=0.1, high=1.0))
+        print(np.random.uniform(low=0.01, high=0.1))
+        print(np.random.uniform(low=0.001, high=0.01))
+        print(np.random.uniform(low=0.0001, high=0.001))
+        # print(np.random.uniform(low=0.00001, high=0.0001))
+        # print(np.random.uniform(low=0.000001, high=0.00001))
+        # print(np.random.uniform(low=0.0000001, high=0.000001))
+        # print(np.random.uniform(low=0.00000001, high=0.0000001))
+
+
 if __name__ == '__main__':
-    a3c_mountaincarv0()
+    a2c_mountaincarv0()
